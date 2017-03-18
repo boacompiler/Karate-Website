24ed79a6a94cba7a67aa8489e842ffe652d53e27
boacompiler <thegeekiestthingonearth@live.co.uk>
2017-03-18 19:40:59 +0000

    register checks existing users to avoid duplication

--- scripts/register.php
+++ scripts/register.php
@@ -1,8 +1,26 @@
 <?php
+    session_start();
     $email=$_POST['email'];
     $password=$_POST['password'];
     $firstname=$_POST['firstname'];
     $secondname=$_POST['secondname'];
     $dob=$_POST['dob'];
-    echo $email . $dob;
+    $conn=new mysqli("localhost","root","password","website"); 
+    if ($conn->connect_error)
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * FROM user WHERE email='$email'";
+    $checkemail = $conn->query($sql);
+    if($checkemail->num_rows >= 1)
+    {
+        echo "uh oh";
+    }
+    else
+    {
+        echo "worked";
+    } 
+    echo "finish";
+    $conn->close();
+    die();
 ?>
