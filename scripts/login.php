0183605f16c708017410a44438493906b0f19ed6
boacompiler <thegeekiestthingonearth@live.co.uk>
2017-03-18 17:24:17 +0000

    starts db and php talking

--- scripts/login.php
+++ scripts/login.php
@@ -1,11 +1,21 @@
 <?php
     session_start();
-    if (isset($_POST['submit']))
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $conn=new mysqli("localhost","root","password","website");
+    if ($conn->connect_error)
     {
-        echo 'it worked';
+        die("Connection failed: " . $conn->connect_error);
     }
-    else
+    $sql = "SELECT * FROM user";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) 
     {
-        echo 'uh oh';
+        while($row = $result->fetch_assoc())
+        {
+            echo $row["namefirst"] . " " . $row["namesecond"];
+        }
     }
+    $conn->close();
 ?> 
