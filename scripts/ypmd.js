b40cac048bc35237c7b7b962d1842eef2c3a292b
boacompiler <thegeekiestthingonearth@live.co.uk>
2017-03-20 15:06:58 +0000

    floating header and working footer

---
+++ scripts/ypmd.js
@@ -0,0 +1,14 @@
+$(document).ready( function()
+{
+    $(window).scroll( function()
+    {
+        if ($(window).scrollTop() > 100)
+        {
+            $('#header').addClass('fixed');
+        }
+        else
+        {
+            $('#header').removeClass('fixed');
+        }
+    });
+});
