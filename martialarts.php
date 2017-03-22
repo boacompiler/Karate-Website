3676fde0b39aff197d058f6529f2e3027d452047
boacompiler <thegeekiestthingonearth@live.co.uk>
2017-03-22 02:36:38 +0000

    works with local images, mysql blobs not so much

--- martialarts.php
+++ martialarts.php
@@ -47,14 +47,15 @@
                 }
                 $sql = "SELECT * FROM class WHERE discipline = 3";
                 $result = $conn->query($sql);
-                echo "<table style=\"width:50%\">";
                 while($row = $result->fetch_assoc())
                 {
+                    echo "<table style=\"width:50%\">";
                     setlocale(LC_MONETARY, 'en_GB.UTF-8');
                     $gbp = money_format('%n',$row['price']);
                     $classid = $row['classid'];
                     echo "<tr style=\"font-weight:bold;\"><td>".$row['name']."</td><td>".$gbp."</td></tr>";    
                     echo "<tr><td colspan=2>".$row['description']."</td></tr>";
+                    echo "</table>";
 
                     $conn2=new mysqli("localhost","root","password","website");
                     if ($conn2->connect_error)
@@ -65,13 +66,15 @@
                     $imageresult = $conn2->query($imagesql);
                     if($imageresult->num_rows > 0)
                     {
+                        echo "<img onclick=\"next".$classid."();return false;\" id=\"gallery".$classid."\"></img>";
                         echo "<script>";
                         echo "var images".$classid." = new Array();";
                         $i = 0;
                         while($imagerow = $imageresult->fetch_assoc())
                         {
                             //echo '<img src="data:image/jpeg;base64,'.base64_encode( $imagerow['image'] ).'"/>';
-                            echo "images".$classid."[".$i."]=".base64_encode( $imagerow['image'] ).";";
+                            //echo "images".$classid."[".$i."]=".base64_encode( $imagerow['image'] ).";";
+                            echo "images".$classid."[".$i."]=\"test.jpg\";";
                             $i++;
                         }
                         echo "var count".$classid."= 0;";
@@ -81,11 +84,9 @@
                         echo "}";
                         echo "document.getElementById(\"gallery".$classid."\").src=images".$classid."[0];";
                         echo "</script>";
-                        echo "<img onclick=\"next".$classid."();return false;\" id=\"gallery".$classid."\"></img>";
                     }
                     $conn2->close();
                 }
-                echo "</table>";
 
                 $conn->close();
             ?>    
