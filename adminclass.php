13208ab2738838ba536ec84689fb7140c3922596
boacompiler <thegeekiestthingonearth@live.co.uk>
2017-05-05 22:09:39 +0100

    adds a class table

---
+++ adminclass.php
@@ -0,0 +1,63 @@
+<?php include 'scripts/base.php'; ?>
+<html>
+    <head>
+        <title>Y/P/M/D</title>
+        <meta charset="UTF-8">
+        <meta name="keywords" content="Yoga, Pilates, Martial Arts, Dance, School, Karate, Tae Kwon Do">
+        <meta name="description" content="Yoga/Pilates/Martial arts/Dance School">
+        <meta name="author" content="Robert Stephens">
+        <link rel="stylesheet" type="text/css" href="ypmd.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <script src="scripts/ypmd.js"></script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header">
+                <h1><a href="index.php">Y/P/M/D</a></h1>
+                <div id="loginindicator">
+                    <?php if(isset($_SESSION['loggedin'])){ ?>
+                        you are logged in as <?php echo $_SESSION['firstname']; ?><br>
+                        <a href="profile.php">Profile</a> <a href="scripts/logoutscript.php">Logout</a>
+                    <?php } else { ?>
+                        you are not logged in <br>
+                        <a href="login.php">Login</a>
+                    <?php } ?>
+                </div>
+                <table id="navTable">
+                    <tr>
+                        <td><a href="yoga.php"><div>Yoga</div></a></td>
+                        <td><p>/</p></td>
+                        <td><a href="pilates.php"><div>Pilates</div></a></td>
+                        <td><p>/</p></td>
+                        <td><a href="martialarts.php"><div>Martial Arts</div></a></td>
+                        <td><p>/</p></td>
+                        <td><a href="dance.php"><div>Dance</div></a></td>
+                    </tr>
+                </table>
+            </div>
+            <div id="content">
+            <h2>Classes</h2>
+            <?php
+                if(isset($_SESSION['loggedin']) and $_SESSION['admin'] == '1')
+                {
+                    include 'scripts/adminclasstable.php';
+                }
+                else
+                {
+            ?>
+            <p>You do not have administrator privileges.</p>
+            <?php } ?>
+            </div>
+            <div id="footer">
+                <table id="footerTable">
+                    <tr>
+                        <td><a href="about.php"><div>About</div></a></td>
+                        <td><a href="contact.php"><div>Contact</div></a></td>
+                        <td><a href="login.php"><div>Login</div></a></td>
+                        <td><a href="admin.php"><div>Admin</div></a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ scripts/adminclasstable.php
@@ -0,0 +1,55 @@
+<?php
+    if(!(isset($_SESSION['loggedin']) and $_SESSION['admin'] == '1'))
+    {
+        die();
+    }
+    $conn=new mysqli($dbhost,$dbuser,$dbpass,$dbname);
+    if($conn->connect_error)
+    {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    $sql = "SELECT * ,d.name as disciplinename,c.name as classname FROM class c INNER JOIN user u ON c.teacher = u.userid INNER JOIN timeslot t ON c.timeslot = t.timeslotid INNER JOIN discipline d ON c.discipline = d.disciplineid;";
+    $studentquery = "SELECT c.classid,COUNT(b.userid) FROM class c LEFT JOIN booking b ON b.classid = c.classid GROUP BY c.classid;";
+    $result = $conn->query($sql);
+    $studentno = $conn->query($studentquery);
+    setlocale(LC_MONETARY, 'en_GB.UTF-8'); //sets the currency to gbp (£) 
+    echo '<table border=1px style="border-collapse:collapse">';
+    echo '<tr style="font-weight:bold;">';
+    echo '<td>Name</td>';
+    echo '<td>Discipline</td>';
+    echo '<td>Teacher</td>';
+    echo '<td>Start</td>';
+    echo '<td>End</td>';
+    echo '<td>Day</td>';
+    echo '<td>Price</td>';
+    echo '<td>Students</td>';
+    echo '</tr>';
+    
+    while($row = $result->fetch_assoc())
+    {
+        $studentcount = $studentno->fetch_assoc();
+        $gbp = money_format('%n',$row['price']);
+        echo '<tr>';
+        echo '<td>'.$row['classname'].'</td>';
+        echo '<td>'.$row['disciplinename'].'</td>';
+        echo '<td>'.$row['namefirst'].' '.$row['namesecond'].'</td>';
+        echo '<td>'.$row['timebegin'].'</td>';
+        echo '<td>'.$row['timeend'].'</td>';
+        echo '<td>'.$row['day'].'</td>';
+        echo '<td>'.$gbp.'</td>';
+        echo '<td>'.$studentcount['COUNT(b.userid)'].'</td>';
+        //edit button
+        echo '<td><form method="post" action="scripts/adminclassedit.php">';
+        echo '<input type="hidden" name="classid" value="'.$row['classid'].'">';
+        echo '<input type="submit" value="Edit"></form></td>';
+        //delet button
+        echo '<td><form method="post" action="scripts/adminclassdelete.php" onsubmit="return confirm(\'Are you sure you want to delete this?\');">';
+        echo '<input type="hidden" name="classid" value="'.$row['classid'].'">';
+        echo '<input type="submit" value="Delete"></form></td>';
+        echo '</tr>';
+    }
+    echo '</table>';
+    echo '<form method="post" action="scripts/adminclassinsert.php">';
+    echo '</form>';
+    $conn->close();
+?>
